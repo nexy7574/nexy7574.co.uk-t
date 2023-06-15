77b8c1baf64b47c186f15af3c22bf81b0f4a9fc5
nex <me@nexy7574.co.uk>
2023-06-15 20:44:43 +0100

    Made front page look cooler

---
+++ src/pages/index.jsx
@@ -0,0 +1,165 @@
+import styles from '@/styles/index.module.css';
+import {useEffect, useState} from "react";
+
+
+function flexLanguages() {
+    alert('I know the following languages: bash/similar, Python 3, HTML+CSS+JS, NextJS, Rust')
+}
+
+function renderWordsTrans(words) {
+    const CLASSES = [
+        styles.trans_blue,
+        styles.trans_pink,
+        styles.trans_white,
+        styles.trans_pink,
+        styles.trans_blue
+    ];
+    let elements = [];
+    for(let i=0; i<5; i++) {
+        elements.push(
+            <span className={CLASSES[i]} key={`${words[i]}-${i}`}>{words[i]}&nbsp;</span>
+        )
+    }
+    return elements
+}
+
+function renderWordsLesbian(words) {
+    const CLASSES = [
+        styles.lesbian_one,
+        styles.lesbian_two,
+        styles.lesbian_three,
+        styles.lesbian_four,
+        styles.lesbian_five,
+        styles.lesbian_six,
+        styles.lesbian_seven,
+    ];
+    let elements = [];
+    for(let i=0; i<7; i++) {
+        elements.push(
+            <span className={CLASSES[i]} key={`${words[i]}-${i}`}>{words[i]}&nbsp;</span>
+        )
+    }
+    return elements
+}
+
+function renderWordsPride(words) {
+    const CLASSES = [
+        styles.pride_one,
+        styles.pride_two,
+        styles.pride_three,
+        styles.pride_four,
+        styles.pride_five,
+        styles.pride_six
+    ];
+    let elements = [];
+    for(let i=0; i<4; i++) {
+        elements.push(
+            <span className={CLASSES[i]} key={`${words[i]}-${i}`}>{words[i]}&nbsp;</span>
+        )
+    }
+    return elements
+}
+
+function renderWordsAce(words) {
+    const CLASSES = [
+        styles.ace_one,
+        styles.ace_two,
+        styles.ace_three,
+        styles.ace_four
+    ];
+    let elements = [];
+    for(let i=0; i<4; i++) {
+        elements.push(
+            <span className={CLASSES[i]} key={`${words[i]}-${i}`}>{words[i]}&nbsp;</span>
+        )
+    }
+    return elements
+}
+
+function renderWords(words) {
+    if(words.length === 4) {
+        return renderWordsAce(words);
+    }
+    else if (words.length === 5) {
+        return renderWordsTrans(words)
+    }
+    else if(words.length === 6) {
+        return renderWordsPride(words);
+    }
+    else {
+        return renderWordsLesbian(words);
+    }
+}
+
+function Link(props) {
+    let text = props.text;
+    let href = props.href;
+    let display_text = text || href;
+    return <li className={styles.linksItem}><a rel={"noopener"} href={href}>[{display_text}]</a></li>
+}
+
+const Div = () => <span className={styles.divider}>|</span>
+
+export default function Home() {
+    const [state, setState] = useState([])
+    const newPhrase = () => {
+        const phrases = [
+            "Actions speak louder than words.",
+            "Live laugh love, womp womp.",
+            "\"Just f*** the hell out\"",
+            "\"Can you hack my instagram?\"",
+            "\"Sorry! No death for you!\"",
+            "Energy companies profit in crisis.",
+            "Medical companies profit in crisis.",
+            "I'm happy you exist.",
+            "\"Step in line, everything will be fine\"",
+            "\"Tell me, how did we get here?\"",
+            "Potato potato potato potato",
+            "Potato potato potato potato potato",
+            "Potato potato potato potato potato potato potato",
+            "Easy to smile when you're pointing the gun"
+        ]
+        const phrase = phrases[Math.floor(Math.random() * phrases.length)];
+        const words = renderWords(phrase.split(/\s+/));
+        setState(words.map((e) => e));
+    }
+    useEffect(
+        newPhrase,
+        []
+    )
+    return (
+        <main className={styles.middleDiv}>
+            <title>My Site</title>
+            <div style={{width: "100%"}}>
+                <div>
+                    <h2 onClick={newPhrase} style={{cursor: "default"}}>
+                        {state.map((e)=>e)}
+                    </h2>
+                </div>
+            </div>
+            <div style={{fontSize: "16px"}}>
+                <span>
+                    17 | she/her |&nbsp;
+                    <span title={"(bash/similar, Python 3, HTML+CSS+JS, NextJS, Rust)"} onClick={flexLanguages} className={styles.fs}>
+                        full-stack
+                    </span>
+                </span>
+            </div>
+            <ul className={styles.links}>
+                <Link href={"//old.nexy7574.co.uk"} text={"Old Site"}/>
+                <Div/>
+                <Link href={"//github.com/EEKIM10/"} text={"GitHub"}/>
+                <Div/>
+                <Link href={"//matrix.to/#/@nex:nexy7574.co.uk"} text={"Contact"}/>
+                <Div/>
+                <Link rel={"//files.nexy7574.co.uk/share/"} text={"File Share"}/>
+                <Div/>
+                <Link rel={"/chat/"} text={"Element Mirror"}/>
+                <Div/>
+                <Link href={"//ferdi-is.gay"} text={"ferdi-is.gay"}/>
+                <Div/>
+                <Link href={"//matthew-is.gay"} text={"matthew-is.gay"}/>
+            </ul>
+        </main>
+    )
+}
